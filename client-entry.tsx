188f114759b72491f24ec15fc667048442fdd7a5
Atsushi Nakatsugawa <atsushi@moongift.co.jp>
2024-06-19 11:07:18 +0900

    init

---
+++ client-entry.tsx
@@ -0,0 +1,39 @@
+import config from './package.json';
+import { QuickChart } from './src/QuickChart';
+import { Options, Func, ViewOptions } from './types/utils';
+
+declare const growiFacade : {
+  markdownRenderer?: {
+    optionsGenerators: {
+      customGenerateViewOptions: (path: string, options: Options, toc: Func) => ViewOptions,
+      generateViewOptions: (path: string, options: Options, toc: Func) => ViewOptions,
+    },
+  },
+};
+
+const activate = (): void => {
+  if (growiFacade == null || growiFacade.markdownRenderer == null) {
+    return;
+  }
+
+  const { optionsGenerators } = growiFacade.markdownRenderer;
+
+  optionsGenerators.customGenerateViewOptions = (...args) => {
+    const options = optionsGenerators.generateViewOptions(...args);
+    const { code } = options.components;
+    options.components.code = QuickChart(code);
+    return options;
+  };
+};
+
+const deactivate = (): void => {
+};
+
+// register activate
+if ((window as any).pluginActivators == null) {
+  (window as any).pluginActivators = {};
+}
+(window as any).pluginActivators[config.name] = {
+  activate,
+  deactivate,
+};
