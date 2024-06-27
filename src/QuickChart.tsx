add9b0570b0e2a283585f59f08b6ff8a613e3de6
Atsushi Nakatsugawa <atsushi@moongift.co.jp>
2024-06-27 22:13:17 +0900

    Add try/catch

--- src/QuickChart.tsx
+++ src/QuickChart.tsx
@@ -4,20 +4,27 @@ const HEIGHT = 'auto';
 
 export const QuickChart = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
   return ({ children, className, ...props }) => {
-    if (className !== 'language-quickchart') {
+    try {
+      if (className !== 'language-quickchart') {
+        return (
+          <Tag {...props}>{children}</Tag>
+        );
+      }
+      const json = JSON.parse(children);
+      const { url, width, height } = json;
+      ['url', 'width', 'height'].forEach(key => delete json[key]);
+      return (
+        <img
+          src={`${url || URL}?c=${JSON.stringify(json)}`}
+          width={width || WIDTH}
+          height={height || HEIGHT}
+        />
+      );
+    }
+    catch (e) {
       return (
         <Tag {...props}>{children}</Tag>
       );
     }
-    const json = JSON.parse(children);
-    const { url, width, height } = json;
-    ['url', 'width', 'height'].forEach(key => delete json[key]);
-    return (
-      <img
-        src={`${url || URL}?c=${JSON.stringify(json)}`}
-        width={width || WIDTH}
-        height={height || HEIGHT}
-      />
-    );
   };
 };
